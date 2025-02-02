5a1bf2f13e5d038586d3f65a7defa8c350a7fdb0
Divyam <divyamkumarp@gmail.com>
2025-02-02 07:31:30 +0530

    Update tax and taxApplicable defaults for Item and SubCategory models

--- models/Item.js
+++ models/Item.js
@@ -6,8 +6,28 @@ const ItemSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   image: { type: String },
   description: { type: String },
-  taxApplicable: { type: Boolean },
-  tax: { type: Number, default: 0 },
+  taxApplicable: { 
+    type: Boolean,
+    default: function() {
+      if (this.subCategoryId && this.subCategoryId.taxApplicable) {
+        return this.subCategoryId.taxApplicable;
+      } else if (this.categoryId && this.categoryId.taxApplicable) {
+        return this.categoryId.taxApplicable;
+      }
+      return false;
+    }
+  },
+  tax: { 
+    type: Number,
+    default: function() {
+      if (this.taxApplicable && this.subCategoryId && this.subCategoryId.tax) {
+        return this.subCategoryId.tax;
+      } else if (this.taxApplicable && this.categoryId && this.categoryId.tax) {
+        return this.categoryId.tax;
+      }
+      return 0;
+    }
+  },
   baseAmount: { type: Number, required: true },
   discount: { type: Number, default: 0 },
   totalAmount: { type: Number }
--- models/SubCategory.js
+++ models/SubCategory.js
@@ -1,12 +1,25 @@
-const mongoose = require('mongoose');
+  const mongoose = require('mongoose');
 
 const SubCategorySchema = new mongoose.Schema({
   categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   name: { type: String, required: true, unique: true },
   image: { type: String },
   description: { type: String },
-  taxApplicable: { type: Boolean },
-  tax: { type: Number }
+  taxApplicable: { type: Boolean, default: function() {
+    if (this.categoryId && this.categoryId.taxApplicable) {
+      return this.categoryId.taxApplicable;
+    }
+    return false;
+  } },
+  tax: {
+    type: Number,
+    default: function () {
+      if (this.taxApplicable && this.categoryId && this.categoryId.tax) {
+        return this.categoryId.tax;
+      }
+      return 0;
+    }
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('SubCategory', SubCategorySchema);
