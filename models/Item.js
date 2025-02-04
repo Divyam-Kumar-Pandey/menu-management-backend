c170dd05e22813b3a729d29394cd65c515d025e8
Divyam <divyamkumarp@gmail.com>
2025-02-04 19:26:34 +0530

    chore: updated schema

--- models/Item.js
+++ models/Item.js
@@ -1,41 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ItemSchema = new mongoose.Schema({
-  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
-  subCategoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'SubCategory', default: null },
-  name: { type: String, required: true, unique: true },
-  image: { type: String },
-  description: { type: String },
-  taxApplicable: { 
-    type: Boolean,
-    default: function() {
-      if (this.subCategoryId && this.subCategoryId.taxApplicable) {
-        return this.subCategoryId.taxApplicable;
-      } else if (this.categoryId && this.categoryId.taxApplicable) {
-        return this.categoryId.taxApplicable;
-      }
-      return false;
-    }
+const ItemSchema = new mongoose.Schema(
+  {
+    categoryId: {type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true},
+    subCategoryId: {type: mongoose.Schema.Types.ObjectId, ref: "SubCategory", default: null},
+    name: {type: String, required: true, unique: true},
+    image: {type: String},
+    description: {type: String},
+    taxApplicable: {
+      type: Boolean,
+      default: false
+    },
+    tax: {
+      type: Number,
+      default: 0
+    },
+    baseAmount: {type: Number, required: true},
+    discount: {type: Number, default: 0},
+    totalAmount: {type: Number}
   },
-  tax: { 
-    type: Number,
-    default: function() {
-      if (this.taxApplicable && this.subCategoryId && this.subCategoryId.tax) {
-        return this.subCategoryId.tax;
-      } else if (this.taxApplicable && this.categoryId && this.categoryId.tax) {
-        return this.categoryId.tax;
-      }
-      return 0;
-    }
-  },
-  baseAmount: { type: Number, required: true },
-  discount: { type: Number, default: 0 },
-  totalAmount: { type: Number }
-}, { timestamps: true });
+  {timestamps: true}
+);
 
 ItemSchema.pre("save", function (next) {
   this.totalAmount = this.baseAmount - this.discount;
   next();
 });
 
-module.exports = mongoose.model('Item', ItemSchema);
+module.exports = mongoose.model("Item", ItemSchema);
--- models/SubCategory.js
+++ models/SubCategory.js
@@ -1,25 +1,23 @@
-  const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const {schema} = require("./Item");
 
 const SubCategorySchema = new mongoose.Schema({
-  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
-  name: { type: String, required: true, unique: true },
-  image: { type: String },
-  description: { type: String },
-  taxApplicable: { type: Boolean, default: function() {
-    if (this.categoryId && this.categoryId.taxApplicable) {
-      return this.categoryId.taxApplicable;
-    }
-    return false;
-  } },
+  categoryId: {type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true},
+  name: {type: String, required: true, unique: true},
+  image: {type: String},
+  description: {type: String},
+  taxApplicable: {type: Boolean, default: false},
   tax: {
     type: Number,
-    default: function () {
-      if (this.taxApplicable && this.categoryId && this.categoryId.tax) {
-        return this.categoryId.tax;
-      }
-      return 0;
-    }
-  }
-}, { timestamps: true });
+    default: 0
+  },
+  timestamps: true
+});
 
-module.exports = mongoose.model('SubCategory', SubCategorySchema);
+SubCategorySchema.pre("save", async function (next) {
+  console.log("Pre save hook");
+  console.log(this.categoryId);
+  this.taxApplicable = this.categoryId.taxApplicable;
+});
+
+module.exports = mongoose.model("SubCategory", SubCategorySchema);
